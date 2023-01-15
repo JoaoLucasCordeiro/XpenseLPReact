c59515ca121200b6125975c4eabf714ae7802195
Lucas Cordeiro <joaolucascttprofissional@gmail.com>
2023-01-15 20:10:07 -0300

    Finished Project

--- src/components/Copyright.js
+++ src/components/Copyright.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
 const Copyright = () => {
-  return <div>Copyright</div>;
+  return (
+    <p className='text-light'>Designer cedido pela <strong className='text-accent'>Ui4Free.</strong> Desenvolvido por <strong className='text-accent'><a target="_blank" href="https://www.linkedin.com/in/joaolucascordeiro/">Lucas Cordeiro</a></strong></p>
+  )
 };
 
 export default Copyright;
